e572b56f61ceb206d06041db86ac51f0282674d5
Zeyo Li <zeyo0510@gmail.com>
2024-06-08 01:19:45 +0000

    init directoryfile struct

---
+++ DirectoryFile-Directory.js
@@ -0,0 +1,12 @@
+((name) => {
+  globalThis.A                 = globalThis.A                 || {};
+  globalThis.A._DirectoryFile_ = globalThis.A._DirectoryFile_ || {};
+  /************************************************/
+  globalThis.A._DirectoryFile_._Directory_ = class $
+  {
+    constructor()
+    {
+
+    }
+  }
+})('DirectoryFile-Directory');
