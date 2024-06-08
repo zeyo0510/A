26c4347fad8747f157d6ae654c8601ecb2b2f258
Zeyo Li <zeyo0510@gmail.com>
2024-06-08 01:23:35 +0000

    init media struct

---
+++ Media-Audio.js
@@ -0,0 +1,12 @@
+((name) => {
+  globalThis.A         = globalThis.A         || {};
+  globalThis.A._Media_ = globalThis.A._Media_ || {};
+  /************************************************/
+  globalThis.A._Media_._Audio_ = class $
+  {
+    constructor()
+    {
+
+    }
+  }
+})('Media-Audio');
---
+++ Media-Video.js
@@ -0,0 +1,12 @@
+((name) => {
+  globalThis.A         = globalThis.A         || {};
+  globalThis.A._Media_ = globalThis.A._Media_ || {};
+  /************************************************/
+  globalThis.A._Media_._Video_ = class $
+  {
+    constructor()
+    {
+
+    }
+  }
+})('Media-Video');
