ae0014abc7a38f00cebdbdf259c862d9742a40af
Zeyo Li <zeyo0510@gmail.com>
2024-06-08 01:12:35 +0000

    init datetime struct

---
+++ DateTime-Time.js
@@ -0,0 +1,12 @@
+((name) => {
+  globalThis.A            = globalThis.A            || {};
+  globalThis.A._DateTime_ = globalThis.A._DateTime_ || {};
+  /************************************************/
+  globalThis.A._DateTime_._Time_ = class $
+  {
+    constructor()
+    {
+
+    }
+  }
+})('DateTime-Time');
