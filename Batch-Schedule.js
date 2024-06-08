706cf51b2dc22644ce92cd30ceafa6a1018c72a4
Zeyo Li <zeyo0510@gmail.com>
2024-06-08 01:11:02 +0000

    update batch format

--- Batch-Schedule.js
+++ Batch-Schedule.js
@@ -1,6 +1,6 @@
 ((name) => {
-  globalThis.A             = globalThis.A         || {};
-  globalThis.A._Batch_     = globalThis.A._Batch_ || {};
+  globalThis.A         = globalThis.A         || {};
+  globalThis.A._Batch_ = globalThis.A._Batch_ || {};
   /************************************************/
   globalThis.A._Batch_._Schedule_ = class $
   {
