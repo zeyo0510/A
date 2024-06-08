538ed2154086e825f98b777d345c619c6d028fc4
Zeyo Li <zeyo0510@gmail.com>
2024-06-08 01:25:06 +0000

    init mousekeybaord struct

---
+++ MouseKeybaord-Keyboard.js
@@ -0,0 +1,12 @@
+((name) => {
+  globalThis.A                 = globalThis.A                 || {};
+  globalThis.A._MouseKeyboard_ = globalThis.A._MouseKeyboard_ || {};
+  /************************************************/
+  globalThis.A._MouseKeyboard_._Keyboard_ = class $
+  {
+    constructor()
+    {
+
+    }
+  }
+})('MouseKeyboard-Keyboard');
---
+++ MouseKeybaord-Mouse.js
@@ -0,0 +1,12 @@
+((name) => {
+  globalThis.A                 = globalThis.A                 || {};
+  globalThis.A._MouseKeyboard_ = globalThis.A._MouseKeyboard_ || {};
+  /************************************************/
+  globalThis.A._MouseKeyboard_._Mouse_ = class $
+  {
+    constructor()
+    {
+
+    }
+  }
+})('MouseKeyboard-Mouse');
